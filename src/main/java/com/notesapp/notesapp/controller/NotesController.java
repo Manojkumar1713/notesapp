a780904c93f5f149d4220e92e6f67e2d2b5ba37b
ManojKumar1713 <manojkumar.mamidi0193@gmail.com>
2021-09-13 22:45:21 +0530

    Added login system

--- src/main/java/com/notesapp/notesapp/controller/NotesController.java
+++ src/main/java/com/notesapp/notesapp/controller/NotesController.java
@@ -1,16 +1,59 @@
 package com.notesapp.notesapp.controller;
 
 import com.notesapp.notesapp.dao.NoresDao;
+import com.notesapp.notesapp.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class NotesController {
 
     @GetMapping("/")
     public String home() throws ClassNotFoundException {
-        NoresDao dao = new NoresDao();
-        System.out.println(dao.getCount());
         return "index";
     }
+    @GetMapping("/register")
+    public String registerUser(Model model){
+        User user = new User();
+        model.addAttribute("user",user);
+        return "registerUser";
+    }
+    @PostMapping("/save")
+    public String saveUser(@ModelAttribute User user,Model model){
+        NoresDao noresDao = new NoresDao();
+        int count = noresDao.getUserName(user.getUsername());
+        if(count == 1){
+            model.addAttribute("badUserName","User Name already existed");
+            return "registerUser";
+        }else {
+            int output = (int) noresDao.insertRecord(user);
+        }
+        return "redirect:/login";
+    }
+    @GetMapping("/login")
+    public String loginUser(Model model){
+        User user = new User();
+        model.addAttribute("user",user);
+        return "loginUser";
+    }
+
+    @PostMapping("/login")
+    public String loginToHome(@ModelAttribute User user, Model model, HttpSession session){
+        NoresDao dao = new NoresDao();
+        int count = dao.validUser(user.getUsername(),user.getPassword());
+        if(count == 1){
+            session.setAttribute("userName",user.getUsername());
+            return "homePage";
+        }
+        else{
+            model.addAttribute("wrongCredentials","UserName or Password is wrong !!");
+            return "loginUser";
+        }
+    }
+
 }
--- src/main/java/com/notesapp/notesapp/dao/NoresDao.java
+++ src/main/java/com/notesapp/notesapp/dao/NoresDao.java
@@ -1,12 +1,10 @@
 package com.notesapp.notesapp.dao;
 
 import com.notesapp.notesapp.dbcon.DbConnection;
+import com.notesapp.notesapp.model.User;
 
 import java.net.URISyntaxException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class NoresDao {
     public Connection connect() throws SQLException, URISyntaxException, ClassNotFoundException {
@@ -14,13 +12,62 @@ public class NoresDao {
     }
 
     public int getCount(){
-        String SQL = "SELECT count(*) FROM public.\"Test\"";
+         String SQL = "SELECT count(*) FROM public.\"Test\"";
         int count = 0;
 
         try {
             Connection conn = connect();
              Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(SQL);
+            System.out.println("Executed ---> "+SQL);
+            rs.next();
+            count = rs.getInt(1);
+        } catch (SQLException | URISyntaxException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return count;
+    }
+    public long insertRecord(User u){
+        long id = 0;
+        String sql = "Insert into public.\"Test\"(name,password) values (?,?)";
+        try{
+            Connection con = connect();
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setString(1,u.getUsername());
+            preparedStatement.setString(2,u.getPassword());
+            id = preparedStatement.executeUpdate();
+            System.out.println("Executed ---> "+sql);
+        }catch(Exception e){
+            System.out.println(e.getStackTrace());
+        }
+        return id;
+    }
+    public int getUserName(String name){
+        String SQL = "SELECT count(*) FROM public.\"Test\" where name ='"+name+"'";
+        int count = 0;
+
+        try {
+            Connection conn = connect();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(SQL);
+            System.out.println("Executed ---> "+SQL);
+            rs.next();
+            count = rs.getInt(1);
+        } catch (SQLException | URISyntaxException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return count;
+    }
+    public int validUser(String name,String password){
+        String SQL = "SELECT count(*) FROM public.\"Test\" where name ='"+name+"' and password ='"+password+"'";
+        int count = 0;
+
+        try {
+            Connection conn = connect();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(SQL);
             System.out.println("Executed ---> "+SQL);
             rs.next();
             count = rs.getInt(1);
